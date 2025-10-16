f2c41be2fa52670a6c84ecb1f1a8161d6978a6ff
strukeee <165663836+strukeee@users.noreply.github.com>
2025-10-16 08:48:46 +0800

    Add files via upload

---
+++ assets/js/load-navbar.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 加载导航栏
+    fetch('../components/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar-container').innerHTML = data;
+
+            // 设置当前活动页面的导航项高亮
+            const currentPage = window.location.pathname.split('/').pop();
+            const navLinks = document.querySelectorAll('.nav-link');
+
+            navLinks.forEach(link => {
+                const linkPage = link.getAttribute('href').split('/').pop();
+                if (linkPage === currentPage ||
+                    (currentPage === '' && linkPage === 'index.html')) {
+                    link.classList.add('active');
+                }
+            });
+        });
+});
